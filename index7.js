068e8a7724347a47185cfbe334d272a3d8b35304
Fern Smith <USER_EMAIL>
2024-10-17 00:36:18 -0400

    first commit

---
+++ index7.js
@@ -0,0 +1,23 @@
+var obj = {
+    numbers: {
+      a: 1,
+      b: 2
+    }
+  };
+  
+  var a = obj.numbers.a;
+  var b = obj.numbers.b;
+
+/* Write an ES2015 Version */
+
+const obj = {
+    numbers: {
+        a: 1,
+        b: 2
+    }
+};
+
+const {a,b} = obj.numbers 
+
+
+
---
+++ index8.js
@@ -0,0 +1,6 @@
+var arr = [1, 2];
+var temp = arr[0];
+arr[0] = arr[1];
+arr[1] = temp;
+
+[arr[0], arr[1]] = [arr[1], arr[0]]
